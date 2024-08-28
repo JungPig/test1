e16fd55906e2aa405e10638595eea2a7a51e5938
JungPig <pig.ohgiraffers@gmail.com>
2024-08-28 17:31:35 +0900

    테스트메시지

--- chap01-team/src/main/java/com/ohgiraffers/test/Application.java
+++ chap01-team/src/main/java/com/ohgiraffers/test/Application.java
@@ -6,6 +6,7 @@ public class Application {
         System.out.println("test2");
         System.out.println("1번테스트");
         System.out.println("2번테스트");
+        System.out.println("3번 테스트");
     }
     public void test(){
         System.out.println("test");
