687e4148a51f5586047568c342310fb1edde96d0
JungPig <pig.ohgiraffers@gmail.com>
2024-08-28 17:12:30 +0900

    testCommit

--- chap01-team/src/main/java/com/ohgiraffers/test/Application.java
+++ chap01-team/src/main/java/com/ohgiraffers/test/Application.java
@@ -3,5 +3,6 @@ package com.ohgiraffers.test;
 public class Application {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test2");
     }
 }
